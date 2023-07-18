39a5571ee49baf447057d68299ab504ccd3b32ed
Enning94 <enningfestus@gmail.com>
2023-07-18 15:33:00 +0000

    add FK indexes, create tables 'invoice_items,treatments', create join table 'treatment_histories'

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -22,3 +22,53 @@ CREATE TABLE invoices (
     paid_at TIMESTAMP,
     medical_history_id INT UNIQUE REFERENCES medical_histories(id)
 );
+
+-- create invoice_items table
+CREATE TABLE invoices_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL(10, 2),
+    quantity INT,
+    total_price DECIMAL(10, 2),
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatment(id)
+   
+);
+-- create treatments table
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255),
+    name VARCHAR(255)
+);
+
+-- create join table 'treatment_histories'
+CREATE TABLE treatments_histories (
+  treatment_id INT REFERENCES treatments (id),
+  medical_histories_id INT REFERENCES medical_histories (id),
+  PRIMARY KEY (  treatment_id, medical_histories_id)
+);
+
+
+--  Add the FK indexes
+-- Create index for medical_histories_id in treatments_histories table
+CREATE INDEX idx_treatments_histories_medical_histories_id
+ON treatments_histories (medical_histories_id);
+
+-- Create index for treatment_id in treatments_histories table
+CREATE INDEX idx_treatments_histories_treatment_id
+ON treatments_histories (treatment_id);
+
+-- Create index for invoice_id in medical_histories table
+CREATE INDEX idx_medical_histories_invoice_id
+ON medical_histories (invoice_id);
+
+-- Create index for medical_history_id in medical_histories table
+CREATE UNIQUE INDEX idx_medical_histories_medical_history_id
+ON medical_histories (medical_history_id);
+
+-- Create index for patient_id in medical_histories table
+CREATE INDEX idx_medical_histories_patient_id
+ON medical_histories (patient_id);
+
+-- Create index for treatment_id in invoices_items table
+CREATE INDEX idx_invoices_items_treatment_id
+ON invoices_items (treatment_id);
